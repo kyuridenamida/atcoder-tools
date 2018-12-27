5e9a08ec2bd2f19be78acc8089c20d0887f9d78e
kyuridenamida <tyotyo3@gmail.com>
2018-12-28 04:49:56 +0900

    autopep8

--- tests/test_tester.py
+++ tests/test_tester.py
@@ -5,9 +5,9 @@ from unittest.mock import patch
 from atcodertools.tools import tester
 from atcodertools.tools.tester import is_executable_file
 
-RESOURCE_DIR = os.path.join(
+RESOURCE_DIR = os.path.abspath(os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
-    "./resources/test_tester/")
+    "./resources/test_tester/"))
 
 
 class TestTester(unittest.TestCase):
