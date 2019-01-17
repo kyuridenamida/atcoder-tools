faa07d6119c505b2b819e6f8b0db1a68220ad5e7
Kazuma Mikami <tyotyo3@gmail.com>
2019-01-18 01:44:30 +0900

    Use pathlib

--- atcodertools/tools/templates/__init__.py
+++ atcodertools/tools/templates/__init__.py
@@ -1,7 +1,7 @@
-import os
+import pathlib
 
-DEFAULT_TEMPLATE_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_TEMPLATE_DIR_PATH = pathlib.Path(__file__).parent
 
 
-def get_default_template_path(lang: str):
-    return os.path.abspath(os.path.join(DEFAULT_TEMPLATE_DIR_PATH, "default_template.{}".format(lang)))
+def get_default_template_path(lang: str) -> str:
+    return str(DEFAULT_TEMPLATE_DIR_PATH / "./default_template.{}".format(lang))
