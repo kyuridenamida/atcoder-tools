c19712f4f2f315ae0788b38e8fbf1f11a94c8592
Kazuma Mikami <tyotyo3@gmail.com>
2019-01-17 22:12:07 +0900

    Remove code & replace abspath to pathlib

--- atcodertools/tools/__init__.py
+++ atcodertools/tools/__init__.py
@@ -1,5 +1,5 @@
-import os
+import pathlib
 
 
 def get_default_config_path():
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "atcodertools-default.toml")
+    return pathlib.Path(pathlib.Path(__file__).parent, "atcodertools-default.toml").resolve()
