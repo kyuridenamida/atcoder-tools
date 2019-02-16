6a60bd969ff8c3ff72a04f1a2052374dacff7eb8
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-02-16 20:00:03 +0900

    Apply suggested changes in the review

--- tests/test_codegen_command.py
+++ tests/test_codegen_command.py
@@ -10,7 +10,7 @@ RESOURCE_DIR = os.path.join(
 TEMPLATE_PATH = os.path.join(RESOURCE_DIR, "template_jinja.cpp")
 
 
-class TestCodeGenCOmmand(unittest.TestCase):
+class TestCodeGenCommand(unittest.TestCase):
 
     def test_generate_code(self):
         answer_data_dir_path = os.path.join(
@@ -18,7 +18,7 @@ class TestCodeGenCOmmand(unittest.TestCase):
             "test_prepare_workspace")
 
         config_path = os.path.join(RESOURCE_DIR, "test_codegen_command.toml")
-        correct_file_path = os.path.join(RESOURCE_DIR, "generated_code.cpp")
+        answer_file_path = os.path.join(RESOURCE_DIR, "generated_code.cpp")
         f1 = io.StringIO()
 
         main(
@@ -32,7 +32,7 @@ class TestCodeGenCOmmand(unittest.TestCase):
             output_file=f1
         )
 
-        with open(correct_file_path) as f2:
+        with open(answer_file_path) as f2:
             self.assertEqual(f1.getvalue(), f2.read())
 
 
