bb03ef66b8b3741e4eae7198847b12f346cd4349
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-02-16 23:02:08 +0900

    Add the "codegen" subcommand (#98)
    * Add the "codegen" subcommand
    
    * Add tests for the "codegen" subcommand
    
    * Fix the wrong returncode of codegen
    
    * Apply suggested changes in the review
    
    * Add a test for the URL parser used at the "codegen" subcommand
    
    * Update README.md for the codegen subcommand

---
+++ tests/resources/test_codegen_command/template_jinja.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+{% if mod is not none %}
+const int mod = {{ mod }};
+{% endif %}
+{% if yes_str is not none %}
+const string YES = "{{ yes_str }}";
+{% endif %}
+{% if no_str is not none %}
+const string NO = "{{ no_str }}";
+{% endif %}
+void solve({{ formal_arguments }}){
+
+}
+int main(){
+    {{input_part}}
+    solve({{ actual_arguments }});
+    return 0;
+}
