c85899b2f3f848febf25e09d4078b34856afaa51
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-02-16 23:04:31 +0900

    Add a code generator for Python 3 (#97)
    * Add a code generator for Python 3
    
    * Add tests for the code generator for Python 3
    
    * Update README.md
    
    * Remove some typehints from default_template.py
    
    * Fix the option of autopep8 in .travis.yml

---
+++ tests/resources/test_codegen/test_default_code_generators_and_templates/python/echo_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sys
+
+{% if mod %}
+MOD = {{ mod }}  # type: int
+{% endif %}
+{% if yes_str %}
+YES = "{{ yes_str }}"  # type: str
+{% endif %}
+{% if no_str %}
+NO = "{{ no_str }}"  # type: str
+{% endif %}
+
+def solve({{ formal_arguments }}):
+    print(N, M)
+    assert len(H) == N - 1
+    for i in range(N - 1):
+        assert len(H[i]) == M - 2
+        print(*H[i])
+    assert len(A) == N - 1
+    assert len(B) == N - 1
+    for i in range(N - 1):
+        print(A[i], B[i])
+    print(Q)
+    assert len(X) == M + Q
+    for i in range(M + Q):
+        print(X[i])
+
+    print(YES)
+    print(NO)
+    print(MOD)
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    {{ input_part }}
+    solve({{ actual_arguments }})
+
+if __name__ == '__main__':
+    main()
