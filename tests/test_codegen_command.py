b4e604d736974aeb5699e5cbe8522f6e3ff79bc2
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-02-16 20:09:56 +0900

    Add a test for the URL parser used at the "codegen" subcommand

--- tests/test_codegen_command.py
+++ tests/test_codegen_command.py
@@ -2,7 +2,9 @@ import io
 import os
 import unittest
 
-from atcodertools.tools.codegen import main
+from atcodertools.client.models.contest import Contest
+from atcodertools.client.models.problem import Problem
+from atcodertools.tools.codegen import main, get_problem_from_url
 
 RESOURCE_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
@@ -34,6 +36,24 @@ class TestCodeGenCommand(unittest.TestCase):
 
         with open(answer_file_path) as f2:
             self.assertEqual(f1.getvalue(), f2.read())
+
+    def test_url_parser(self):
+        dummy_alphabet = "Z"
+        problem = Problem(Contest("utpc2014"), "Z", "utpc2014_k")
+        urls = [
+            "http://utpc2014.contest.atcoder.jp/tasks/utpc2014_k",
+            "http://beta.atcoder.jp/contests/utpc2014/tasks/utpc2014_k",
+            "http://atcoder.jp/contests/utpc2014/tasks/utpc2014_k",
+            "https://utpc2014.contest.atcoder.jp/tasks/utpc2014_k",
+            "https://beta.atcoder.jp/contests/utpc2014/tasks/utpc2014_k",
+            "https://atcoder.jp/contests/utpc2014/tasks/utpc2014_k",
+            "https://atcoder.jp/contests/utpc2014/tasks/utpc2014_k?lang=en",
+            "https://atcoder.jp/contests/utpc2014/tasks/utpc2014_k/?lang=en",
+        ]
+
+        for url in urls:
+            self.assertEqual(
+                get_problem_from_url(url).to_dict(), problem.to_dict())
 
 
 if __name__ == '__main__':
