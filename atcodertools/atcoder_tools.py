30e9562fb79fb5550e9e0bbd6323887643b47ecb
kotamanegi <kotamanegi84@gmail.com>
2019-03-05 00:44:14 +0900

    autopep8

--- atcodertools/atcoder_tools.py
+++ atcodertools/atcoder_tools.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import sys
 
-from atcodertools.release_management.version_check import get_latest_version, VersionCheckError
+from atcodertools.release_management.version_check import (
+    get_latest_version,
+    VersionCheckError,
+)
 from atcodertools.tools.envgen import main as envgen_main
 from atcodertools.tools.tester import main as tester_main
 from atcodertools.tools.submit import main as submit_main
@@ -59,6 +62,7 @@ def main():
 
     if sys.argv[1] == "codegen":
         codegen_main(prog, args)
+
 
 if __name__ == '__main__':
     main()
