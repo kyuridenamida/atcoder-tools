42999135fda32a890f582ffa555cb5f788e8f8fb
kitagawa-hr <kitagawa@cancerscan.jp>
2019-05-29 15:46:00 +0900

    Add in_ and out_example_format to EtcConfig (#140)
    envgen.pyにおいてIN_EXAMPLE_FORMAT,
      OUT_EXAMPLE_FORMATとして定義されていたテストケースのフォーマットを
      configファイルで設定できるように変更.

--- atcodertools/config/etc_config.py
+++ atcodertools/config/etc_config.py
@@ -4,7 +4,11 @@ class EtcConfig:
                  download_without_login: str = False,
                  parallel_download: bool = False,
                  save_no_session_cache: bool = False,
+                 in_example_format: str = "in_{}.txt",
+                 out_example_format: str = "out_{}.txt",
                  ):
         self.download_without_login = download_without_login
         self.parallel_download = parallel_download
         self.save_no_session_cache = save_no_session_cache
+        self.in_example_format = in_example_format
+        self.out_example_format = out_example_format
