42063a6f4c4b0477610480fa99fd5f9ae11a5a9c
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-06-07 18:55:30 +0900

    Remove tests./test_fmtprediction.TestFormatPrediction.test_overall

--- tests/test_fmtprediction.py
+++ tests/test_fmtprediction.py
@@ -1,14 +1,12 @@
 import logging
-import tempfile
 import unittest
-import os
+from typing import Any, List, Optional, Dict
 
-from tests.utils.gzip_controller import make_tst_data_controller
-from tests.utils.fmtprediction_test_runner import FormatPredictionTestRunner
+from onlinejudge.service.atcoder import AtCoderProblem
 
-ANSWER_FILE = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    './resources/test_fmtprediction/answer.txt')
+from atcodertools.client.atcoder import AtCoderClient
+from atcodertools.fmtprediction.predict_format import predict_format, NoPredictionResultError
+
 
 fmt = "%(asctime)s %(levelname)s: %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=fmt)
@@ -16,47 +14,117 @@ logging.basicConfig(level=logging.DEBUG, format=fmt)
 
 class TestFormatPrediction(unittest.TestCase):
 
-    def setUp(self):
-        self.temp_dir = tempfile.mkdtemp()
-        self.test_data_controller = make_tst_data_controller(
-            tempfile.mkdtemp())
-        self.test_dir = self.test_data_controller.create_dir()
+    def setUp(self) -> None:
+        self.client = AtCoderClient()
 
-    def tearDown(self):
-        self.test_data_controller.remove_dir()
+    def predict_format(self, url: str) -> Optional[Dict[str, Any]]:
+        problem = AtCoderProblem.from_url(url)
+        content = self.client.download_problem_content(problem)
+        try:
+            result = predict_format(content)
+            print(result.format)
+            return {
+                'format': str(result.format),
+                'type': {var.name: var.type.to_py_type() for var in result.format.all_vars()},
+            }
+        except NoPredictionResultError:
+            return None
 
-    def test_overall(self):
-        runner = FormatPredictionTestRunner(self.test_dir)
-        case_names = sorted(
-            [cand for cand in os.listdir(self.test_dir) if runner.is_valid_case(cand)])
-        output_text = ""
-        for case in case_names:
-            response = runner.run(case)
+    def test_variables_with_long_names(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/arc001/tasks/arc001_4"), {
+            'format': '[(Singular: N),(Singular: start),(Singular: goal),(Parallel: l,r | 0 to N)]',
+            'type': {'N': int, 'start': int, 'goal': int, 'l': int, 'r': int},
+        })
 
-            if response.status == "OK":
-                output_text += "{:40} {:20} {} {}\n".format(case, response.status, response.simple_format,
-                                                            response.types)
-            else:
-                output_text += "{:40} {}\n".format(case, response.status)
+    def test_an_old_problem(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/abc002/tasks/abc002_2"), {
+            'format': '[(Singular: W)]',
+            'type': {'W': str},
+        })
 
-        with open(ANSWER_FILE, 'r') as f:
-            answer = f.read()
+    def test_parallel_to_2n(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/agc001/tasks/agc001_a"), {
+            'format': '[(Singular: N),(Parallel: L | 1 to 2*N)]',
+            'type': {'N': int, 'L': int},
+        })
 
-        for ans, out in zip(answer.split("\n"), output_text.split("\n")):
-            if ans != out:
-                # Case name is expected to be stored to the first column in the
-                # file.
-                case_name = ans.split()[0]
-                content = runner.load_problem_content(case_name)
-                logging.debug("=== {} ===".format(case_name))
-                logging.debug(
-                    "Input Format:\n{}".format(content.input_format_text))
-                for idx, s in enumerate(content.samples):
-                    logging.debug(
-                        "Sample Input {num}:\n{inp}".format(inp=s.get_input(), num=idx + 1))
-                self.assertEqual(ans, out)
+    def test_parallel_from_1_to_1(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/agc007/tasks/agc007_c"), {
+            'format': '[(Singular: N),(Parallel: d | 1 to 1),(Singular: x)]',
+            'type': {'N': int, 'd': int, 'x': int},
+        })
 
-        self.assertEqual(len(answer), len(output_text))
+    def test_parallel_with_two_variables(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/agc015/tasks/agc015_e"), {
+            'format': '[(Singular: N),(Parallel: X,V | 1 to N)]',
+            'type': {'N': int, 'X': int, 'V': int},
+        })
+
+    def test_matrix_of_char(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/agc028/tasks/agc028_f"), {
+            'format': '[(Singular: N),(Parallel: A | 1 to N)]',
+            'type': {'N': int, 'A': str},
+        })
+
+    @unittest.expectedFailure
+    def test_matrix_of_char_failure(self) -> None:
+        self.assertIsNotNone(self.predict_format(
+            "https://atcoder.jp/contests/agc004/tasks/agc004_c"))
+
+    @unittest.expectedFailure
+    def test_long_digits(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/arc088/tasks/arc088_b"), {
+            'format': '[(Singular: S)]',
+            'type': {'S': str},
+        })
+
+    def test_float_type(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/arc015/tasks/arc015_4"), {
+            'format': '[(Singular: T),(Singular: N),(Singular: P),(Parallel: q,x,t | 1 to N)]',
+            'type': {'T': int, 'N': int, 'P': float, 'q': float, 'x': int, 't': int},
+        })
+
+    def test_char_indexed(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/agc008/tasks/agc008_c"), {
+            'format': '[(Singular: a_I),(Singular: a_O),(Singular: a_T),(Singular: a_J),(Singular: a_L),(Singular: a_S),(Singular: a_Z)]',
+            'type': {'a_I': int, 'a_O': int, 'a_T': int, 'a_J': int, 'a_L': int, 'a_S': int, 'a_Z': int},
+        })
+
+    @unittest.expectedFailure
+    def test_array_of_char_without_spaces(self) -> None:
+        self.assertIsNotNone(self.predict_format(
+            "https://atcoder.jp/contests/arc097/tasks/arc097_d"), None)
+
+    @unittest.expectedFailure
+    def test_two_steps_parallel(self) -> None:
+        self.assertIsNotNone(self.predict_format(
+            "https://atcoder.jp/contests/arc096/tasks/arc096_d"), None)
+
+    @unittest.expectedFailure
+    def test_not_space_separated(self) -> None:
+        self.assertIsNotNone(self.predict_format(
+            "https://atcoder.jp/contests/arc002/tasks/arc002_2"))
+
+    @unittest.expectedFailure
+    def test_complicated_input_format(self) -> None:
+        self.assertIsNotNone(self.predict_format(
+            "https://atcoder.jp/contests/agc029/tasks/agc029_f"))
+
+    @unittest.expectedFailure
+    def test_two_pre_tags(self) -> None:
+        self.assertIsNotNone(self.predict_format(
+            "https://atcoder.jp/contests/agc027/tasks/agc027_f"))
+
+    def test_complicated_variable_names(self) -> None:
+        self.assertEqual(self.predict_format("https://atcoder.jp/contests/abc022/tasks/abc022_d"), {
+            'format': '[(Singular: N),(Parallel: Ax,Ay | 1 to N),(Parallel: Bx,By | 1 to N)]',
+            'type': {'N': int, 'Ax': int, 'Ay': int, 'Bx': int, 'By': int},
+        })
+
+    @unittest.expectedFailure
+    def test_lists_with_heterogeneous_lengths(self) -> None:
+        self.assertIsNotNone(self.predict_format(
+            "https://atcoder.jp/contests/iroha2019-day1/tasks/iroha2019_day1_k"))
 
 
 if __name__ == "__main__":
