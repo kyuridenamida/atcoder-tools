1c53b80068df2ce07c9b5139009c5be116d617d9
poman <poman1638m@gmail.com>
2019-08-04 23:13:01 +0900

    クラス名を修正

--- atcodertools/codegen/code_generators/d.py
+++ atcodertools/codegen/code_generators/d.py
@@ -23,7 +23,7 @@ def _loop_header(var: Variable, for_second_index: bool):
     )
 
 
-class CppCodeGenerator:
+class DlangCodeGenerator:
 
     def __init__(self,
                  format_: Optional[Format[Variable]],
@@ -164,7 +164,7 @@ class NoPredictionResultGiven(Exception):
 
 
 def main(args: CodeGenArgs) -> str:
-    code_parameters = CppCodeGenerator(
+    code_parameters = DlangCodeGenerator(
         args.format, args.config).generate_parameters()
     return render(
         args.template,
