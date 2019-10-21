af75e4994c6cec3e8f3bd652db54c34c55cfdb35
chaemon <chaemon@gmail.com>
2019-10-21 15:36:58 +0900

    judge_templateを追加

--- atcodertools/tools/judgetype_setter.py
+++ atcodertools/tools/judgetype_setter.py
@@ -2,13 +2,16 @@
 
 import argparse
 import os
+import shutil
 from atcodertools.common.judgetype import ErrorType, NormalJudge, DecimalJudge, MultiSolutionJudge, InteractiveJudge, JudgeType, NoJudgeTypeException, DEFAULT_EPS
 from atcodertools.tools.models.metadata import Metadata
+from atcodertools.common.language import Language, ALL_LANGUAGES
+from atcodertools.tools.templates import get_default_judge_template_path
 
 
 def main(prog, args):
     if len(args) == 0:
-        print("Usage: ")
+        print("Usage: atcoder tools set [options]")
         return
     new_judge_type = args[0]
 
@@ -30,6 +33,11 @@ def main(prog, args):
                              ' must be one of [{}]'.format(
                                  ', '.join([x.value for x in list(ErrorType)])),
                         type=str,
+                        default=None)
+
+    parser.add_argument("--lang",
+                        help="Programming language of your template code, {}.\n".format(
+                            " or ".join([lang.name for lang in ALL_LANGUAGES])),
                         default=None)
 
     args = parser.parse_args(args[1:])
@@ -54,12 +62,19 @@ def main(prog, args):
     elif new_judge_type == JudgeType.MultiSolution.value:
         if not os.path.exists("./judge.cpp"):
             print("touch ./judge.cpp (multi sotlution)")
+            judge_template_path = get_default_judge_template_path('cpp')
+            shutil.copy(judge_template_path, "./judge.cpp")
         else:
             print("Judge Code exists")
     elif new_judge_type == JudgeType.Interactive.value:
-        if not os.path.exists("./judge.cpp"):
+        if not os.path.exists("/judge.cpp"):
             print("touch ./judge.cpp (interactive)")
+            judge_template_path = get_default_judge_template_path('cpp')
+            shutil.copy(judge_template_path, "./judge.cpp")
         else:
             print("Judge Code exists")
-
+    if args.lang is not None:
+        if args.lang != metadata.lang.name:
+            metadata.lang = Language.from_name(args.lang)
+            # TODO: generate code
     metadata.save_to("./metadata.json")
--- atcodertools/tools/templates/default_judge_template.cpp
+++ atcodertools/tools/templates/default_judge_template.cpp
@@ -1,20 +1,19 @@
-/////////start template
 #include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long long Int;
 
-void quitAC(){
+void exit_ac(){
 	cerr<<"Judge: AC"<<endl;
 	exit(0);
 }
-void quitWA(const string message = ""){
+void exit_wa(const string message = ""){
 	cerr<<"Judge: WA"<<" ( "<<message<<" )"<<endl;
 	exit(1);
 }
 
-///// only interactive
+#ifdef INTERACTIVE
 const string header_prefix = "Input                 Output\n----------------------------------";
 const string input_prefix  = "                      ";
 
@@ -29,15 +28,12 @@ void output(const string &s){
 	cerr<<s<<endl;
 	cout<<s<<endl;
 }
-///// end only interactive
-
-///////////////////end template
+#endif
 
 typedef long long Int;
 
 int main(int argc, char *argv[]){
-	//////////////// start template
-	///// only interactive
+#ifdef INTERACTIVE
 	cerr<<header_prefix<<endl;
 	ifstream in_s_2(argv[1]);
 	while(in_s_2){
@@ -46,8 +42,11 @@ int main(int argc, char *argv[]){
 		cout<<s<<endl;
 		cerr<<s<<endl;
 	}
-	///// end only interactive
+#endif
 	ifstream in_s(argv[1]), out_s(argv[2]);
-	//////////////// end template
+	//write code
+	
+	
+	
+	assert(false);
 }
-
