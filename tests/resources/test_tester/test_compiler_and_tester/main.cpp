181caa40dc7a6f979f72853a56774e5a0ef10849
chaemon <chaemon@gmail.com>
2019-11-07 00:15:35 +0900

    add test_compiler_and_tester

---
+++ tests/resources/test_tester/test_compiler_and_tester/main.cpp
@@ -0,0 +1,57 @@
+// #includes {{{
+#ifdef MY_DEBUG
+#include "header/header.hpp"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(int)(n);++i)
+#define RREP(i,a,b) for(int i=(int)(a);i<(int)(b);++i)
+#define FOR(i,c) for(__typeof((c).begin()) i=(c).begin();i!=(c).end();++i)
+#define LET(x,a) __typeof(a) x(a)
+//#define IFOR(i,it,c) for(__typeof((c).begin())it=(c).begin();it!=(c).end();++it,++i)
+#define ALL(c) (c).begin(), (c).end()
+#define MP make_pair
+
+#define EXIST(e,s) ((s).find(e)!=(s).end())
+
+#define RESET(a) memset((a),0,sizeof(a))
+#define SET(a) memset((a),-1,sizeof(a))
+#define PB push_back
+#define DEC(it,command) __typeof(command) it=command
+
+//debug
+
+#define whole(f,x,...) ([&](decltype((x)) whole) { return (f)(begin(whole), end(whole), ## __VA_ARGS__); })(x)
+
+typedef long long Int;
+typedef unsigned long long uInt;
+typedef long double rn;
+
+template<class T>
+constexpr T inf = numeric_limits<T>::has_infinity ? numeric_limits<T>::infinity():(numeric_limits<T>::max()/2);
+
+int __inf_ignore(){
+	int t = inf<int>;
+	return t;
+}
+
+typedef pair<int,int> pii;
+
+const string nl = "\n";
+
+#ifdef MY_DEBUG
+#include"print.hpp"
+#include"debug.hpp"
+#endif
+// }}}
+
+int main(){
+	int A, B, C;
+	cin>>A>>B>>C;
+	if(A + B >= C)cout<<"Yes"<<endl;
+	else cout<<"No"<<endl;
+}
+
---
+++ tests/resources/test_tester/test_compiler_and_tester/main.nim
@@ -0,0 +1,35 @@
+#{{{ header
+import algorithm, sequtils, tables, macros, math, sets, strutils
+when defined(MYDEBUG):
+  import header
+
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc getchar(): char {.header: "<stdio.h>", varargs.}
+proc nextInt(): int = scanf("%lld",addr result)
+proc nextFloat(): float = scanf("%lf",addr result)
+proc nextString(): string =
+  var get = false
+  result = ""
+  while true:
+    var c = getchar()
+    if int(c) > int(' '):
+      get = true
+      result.add(c)
+    else:
+      if get: break
+      get = false
+template `max=`*(x,y:typed):void = x = max(x,y)
+template `min=`*(x,y:typed):void = x = min(x,y)
+template infty(T): untyped = ((T(1) shl T(sizeof(T)*8-2)) - 1)
+#}}}
+
+proc main():void =
+  let
+    A = nextInt()
+    B = nextInt()
+    C = nextInt()
+  if A + B >= C: echo "Yes"
+  else: echo "No"
+
+main()
+
