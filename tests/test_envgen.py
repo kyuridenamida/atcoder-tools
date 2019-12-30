25795ec1154746df099cf74678b7874c13a2c329
Yuki Hamada <yuuki3655@gmail.com>
2019-12-30 03:38:56 -0800

    Makes gen command retry with exponential backoff (#174)
    Previously, gen command retries with fixed delay of 1.5 seconds indefinitely. It could hurt the service especially when they are experiencing overloaded traffic. This patch changes the retry strategy with exponential backoff starting from 1.5 seconds to 60 seconds at maximum. The gen commands aborts by EnvironmentInitializationError exception after 10 attempts.

--- tests/test_envgen.py
+++ tests/test_envgen.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import tempfile
 import unittest
+from unittest import mock
 from os.path import relpath
 from logging import getLogger
 
@@ -9,7 +10,7 @@ from atcodertools.client.atcoder import AtCoderClient
 from atcodertools.codegen.code_style_config import CodeStyleConfig
 from atcodertools.config.config import Config
 from atcodertools.config.etc_config import EtcConfig
-from atcodertools.tools.envgen import prepare_contest, main
+from atcodertools.tools.envgen import prepare_contest, main, EnvironmentInitializationError
 
 logger = getLogger(__name__)
 
@@ -75,6 +76,38 @@ class TestEnvGen(unittest.TestCase):
                     ))
             )
         self.assertDirectoriesEqual(answer_data_dir_path, self.temp_dir)
+
+    @mock.patch('time.sleep')
+    def test_prepare_contest_aborts_after_max_retry_attempts(self, mock_sleep):
+        mock_client = mock.Mock(spec=AtCoderClient)
+        mock_client.download_problem_list.return_value = []
+        self.assertRaises(
+            EnvironmentInitializationError,
+            prepare_contest,
+            mock_client,
+            "agc029",
+            Config(
+                code_style_config=CodeStyleConfig(
+                    workspace_dir=self.temp_dir,
+                    template_file=TEMPLATE_PATH,
+                    lang="cpp",
+                ),
+                etc_config=EtcConfig(
+                    in_example_format="input_{}.txt",
+                    out_example_format="output_{}.txt"
+                ))
+        )
+        self.assertEqual(mock_sleep.call_count, 10)
+        mock_sleep.assert_has_calls([mock.call(1.5),
+                                     mock.call(3.0),
+                                     mock.call(6.0),
+                                     mock.call(12.0),
+                                     mock.call(24.0),
+                                     mock.call(48.0),
+                                     mock.call(60.0),
+                                     mock.call(60.0),
+                                     mock.call(60.0),
+                                     mock.call(60.0)])
 
     def assertDirectoriesEqual(self, expected_dir_path, dir_path):
         files1 = get_all_rel_file_paths(expected_dir_path)
