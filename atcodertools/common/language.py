4595b8b10369b1b6a837428f2d4cb446a43a4888
Kazuma Mikami <tyotyo3@gmail.com>
2020-05-17 03:20:59 +0900

    Update atcodertools/common/language.py
    Co-authored-by: DNEK <dnek.app@gmail.com>

--- atcodertools/common/language.py
+++ atcodertools/common/language.py
@@ -82,7 +82,7 @@ CPP = Language(
     name="cpp",
     display_name="C++",
     extension="cpp",
-    submission_lang_pattern=re.compile(".*C\\+\\+ \\(GCC.*"),
+    submission_lang_pattern=re.compile(".*C\\+\\+ \\(GCC 9.*|.*C\\+\\+14 \\(GCC 5.*"),
     default_code_generator=cpp.main,
     default_template_path=get_default_template_path('cpp'),
     compile_command="g++ {filename}.cpp -o {filename} -std=c++17",
