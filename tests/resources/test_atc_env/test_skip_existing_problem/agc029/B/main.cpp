cf8b4d43a0e6b08b5d897c4895e2f2cd4cdc3e77
Kohei Namba <dnek.app@gmail.com>
2021-04-22 04:24:24 +0900

    add test for skip existing problem

---
+++ tests/resources/test_atc_env/test_skip_existing_problem/agc029/B/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(long long N, std::vector<long long> A){
+
+}
+
+int main(){
+    long long N;
+    scanf("%lld",&N);
+    std::vector<long long> A(N);
+    for(int i = 0 ; i < N ; i++){
+        scanf("%lld",&A[i]);
+    }
+    solve(N, std::move(A));
+    return 0;
+}
---
+++ tests/resources/test_atc_env/test_skip_existing_problem/agc029/F/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+
+}
+
+int main(){
+    
+    solve();
+    return 0;
+}
