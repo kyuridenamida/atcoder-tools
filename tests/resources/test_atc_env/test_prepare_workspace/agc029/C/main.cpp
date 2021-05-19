4e250725c0f91f99a4bd3592f6abb9eba9a54a74
naskya <m@naskya.net>
2021-05-19 20:35:16 +0900

    Improvement in universal_generator/cpp.toml (#230)
    * Fix typo
    
    * Qualify scanf function with std namespace
    
    * Use long double for float arguments for consistency
    
    * Reflect the changes in universal_generator/cpp.toml
    
    * Reflect the changes in universal_generator/cpp.toml

--- tests/resources/test_atc_env/test_prepare_workspace/agc029/C/main.cpp
+++ tests/resources/test_atc_env/test_prepare_workspace/agc029/C/main.cpp
@@ -25,10 +25,10 @@ void solve(long long N, std::vector<long long> A){
 
 int main(){
     long long N;
-    scanf("%lld", &N);
+    std::scanf("%lld", &N);
     std::vector<long long> A(N);
     for(int i = 0 ; i < N ; i++){
-        scanf("%lld", &A[i]);
+        std::scanf("%lld", &A[i]);
     }
     solve(N, std::move(A));
     return 0;
--- tests/resources/test_codegen_command/generated_code.cpp
+++ tests/resources/test_codegen_command/generated_code.cpp
@@ -23,16 +23,16 @@ void solve(long long N, long long M, std::vector<long long> a, std::vector<long 
 }
 int main(){
     long long N;
-    scanf("%lld", &N);
+    std::scanf("%lld", &N);
     long long M;
-    scanf("%lld", &M);
+    std::scanf("%lld", &M);
     std::vector<long long> a(M);
     std::vector<long long> b(M);
     std::vector<long long> t(M);
     for(int i = 0 ; i < M ; i++){
-        scanf("%lld", &a[i]);
-        scanf("%lld", &b[i]);
-        scanf("%lld", &t[i]);
+        std::scanf("%lld", &a[i]);
+        std::scanf("%lld", &b[i]);
+        std::scanf("%lld", &t[i]);
     }
     solve(N, M, std::move(a), std::move(b), std::move(t));
     return 0;
