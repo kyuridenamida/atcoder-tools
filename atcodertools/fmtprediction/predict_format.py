e92926164af2c1e0eaf2295737cd2189697aeaea
chaemon <chaemon@gmail.com>
2022-01-15 19:03:54 +0900

    1d_flagを2^n通り試す

--- atcodertools/fmtprediction/predict_format.py
+++ atcodertools/fmtprediction/predict_format.py
@@ -31,10 +31,12 @@ def predict_format(content: ProblemContent) -> FormatPredictionResult:
 
     output_cands = []
     for format in tokenized_possible_formats:
-        for to_1d_flag in [False, True]:
+        try:
+            simple_format_array = predict_simple_format(format.var_tokens)
+        except (TypePredictionFailedError, SimpleFormatPredictionFailedError):
+            continue
+        for simple_format in simple_format_array:
             try:
-                simple_format = predict_simple_format(
-                    format.var_tokens, to_1d_flag)
                 output_cands.append(
                     FormatPredictionResult.create_typed_format(simple_format, predict_types(simple_format, samples)))
                 break
--- atcodertools/fmtprediction/predict_simple_format.py
+++ atcodertools/fmtprediction/predict_simple_format.py
@@ -26,7 +26,7 @@ def _predict_period(seq: List[int]):
         return 1
 
 
-def _predict_simple_format_main(var_tokens: List[VariableToken], to_1d_flag=False) -> Format[SimpleVariable]:
+def _predict_simple_format_main(var_tokens: List[VariableToken], to_1d_flag) -> Format[SimpleVariable]:
     var_to_positions = {}
     var_to_simple_var = OrderedDict()
 
@@ -62,14 +62,14 @@ def _predict_simple_format_main(var_tokens: List[VariableToken], to_1d_flag=Fals
 
         dim = var_token.dim_num()
 
-        if to_1d_flag:
+        if pos in to_1d_flag:
             if dim == 2:
                 # simple_var.first_index = simple_var.second_index
                 simple_var.second_index = None
                 dim = 1
-            # elif dim == 1:
-            #     simple_var.first_index = None
-            #     dim = 0
+            elif dim == 1:
+                simple_var.first_index = None
+                dim = 0
 
         if dim == 0:
             root.push_back(SingularPattern(simple_var))
@@ -92,8 +92,78 @@ def _predict_simple_format_main(var_tokens: List[VariableToken], to_1d_flag=Fals
     return root
 
 
-def predict_simple_format(var_tokens: List[VariableToken], to_1d_flag=False) -> Format[SimpleVariable]:
-    try:
-        return _predict_simple_format_main(var_tokens, to_1d_flag)
-    except (WrongGroupingError, UnknownPeriodError):
-        raise SimpleFormatPredictionFailedError
+def _predict_1d_flag_pos(var_tokens: List[VariableToken]) -> List[Format[SimpleVariable]]:
+    var_to_positions = {}
+    var_to_simple_var = OrderedDict()
+
+    # Pre-computation of the min / max value of each of the first and second
+    # indices.
+    for pos, var_token in enumerate(var_tokens):
+        var_name = var_token.var_name
+
+        if var_name not in var_to_simple_var:
+            var_to_simple_var[var_name] = SimpleVariable.create(
+                var_name, var_token.dim_num())
+            var_to_positions[var_name] = []
+
+        var_to_positions[var_name].append(pos)
+
+        if var_token.dim_num() >= 2:
+            var_to_simple_var[var_name].second_index.update(
+                var_token.second_index)
+        if var_token.dim_num() >= 1:
+            var_to_simple_var[var_name].first_index.update(
+                var_token.first_index)
+
+    # Building format nodes
+    already_processed_vars = set()
+
+    result = []
+    for pos, var_token in enumerate(var_tokens):
+        var_name = var_token.var_name
+        # simple_var = var_to_simple_var[var_name]
+
+        if var_name in already_processed_vars:
+            continue
+
+        dim = var_token.dim_num()
+
+        if dim == 0:
+            pass
+        elif dim == 1:
+            try:
+                period = _predict_period(var_to_positions[var_name])
+            except UnknownPeriodError:
+                continue
+            if period == 1:
+                result.append(pos)
+            parallel_vars_group = [var_to_simple_var[token.var_name]
+                                   for token in var_tokens[pos:pos + period]]
+            # try:
+            #     root.push_back(ParallelPattern(parallel_vars_group))
+            # except WrongGroupingError:
+            #     raise
+            for var in parallel_vars_group:
+                already_processed_vars.add(var.name)
+        elif dim == 2:
+            result.append(pos)
+        else:
+            raise NotImplementedError
+        already_processed_vars.add(var_name)
+    return result
+
+
+def predict_simple_format(var_tokens: List[VariableToken]) -> Format[SimpleVariable]:
+    flag_pos = _predict_1d_flag_pos(var_tokens)
+    result = []
+    for b in range(1 << len(flag_pos)):
+        to_1d_flag = set()
+        for i in range(len(flag_pos)):
+            if b & (1 << i):
+                to_1d_flag.add(flag_pos[i])
+        try:
+            result.append(_predict_simple_format_main(var_tokens, to_1d_flag))
+        except (WrongGroupingError, UnknownPeriodError):
+            continue
+            # raise SimpleFormatPredictionFailedError
+    return result
