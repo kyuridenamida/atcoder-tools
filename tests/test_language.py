d6ec258dd5cef7a46326b150f6703b7bd088a283
firewood <174658+firewood@users.noreply.github.com>
2025-11-02 17:55:08 +0900

    update test

--- tests/test_language.py
+++ tests/test_language.py
@@ -6,7 +6,25 @@ from atcodertools.common.language import CPP, JAVA, RUST, PYTHON, DLANG, NIM, CS
 class TestLanguage(unittest.TestCase):
     def test_compilers(self):
         language_compiler_map = {
-            CPP: 'C++ 20 (gcc 12.2)',
+            CPP: 'C++23 (GCC 15.2.0)',
+            JAVA: 'Java24 (OpenJDK 24.0.2)',
+            RUST: 'Rust (rustc 1.89.0)',
+            PYTHON: 'Python (CPython 3.13.7)',
+            DLANG: 'D (DMD 2.111.0)',
+            NIM: 'Nim (Nim 1.6.20)',
+            CSHARP: 'C# 13.0 (.NET 9.0.8)',
+            SWIFT: 'Swift 6.2',
+            GO: 'Go (go 1.25.1)',
+            JULIA: 'Julia (Julia 1.11.6)',
+        }
+        for language, compiler in language_compiler_map.items():
+            self.assertRegex(compiler, language.submission_lang_pattern)
+
+    # ABC429以前
+    # TODO: 新ジャッジのみになったら消す
+    def test_previous_compilers(self):
+        language_compiler_map = {
+            CPP: 'C++ 23 (gcc 12.2)',
             JAVA: 'Java (OpenJDK 17)',
             RUST: 'Rust (rustc 1.70.0)',
             PYTHON: 'Python (CPython 3.11.4)',
@@ -16,23 +34,6 @@ class TestLanguage(unittest.TestCase):
             SWIFT: 'Swift (swift 5.8.1)',
             GO: 'Go (go 1.20.6)',
             JULIA: 'Julia (Julia 1.9.2)',
-        }
-        for language, compiler in language_compiler_map.items():
-            self.assertRegex(compiler, language.submission_lang_pattern)
-
-    # TODO: 新ジャッジのみになったら消す
-    def test_previous_compilers(self):
-        language_compiler_map = {
-            CPP: 'C++ (GCC 9.2.1)',
-            JAVA: 'Java (OpenJDK 11.0.6)',
-            RUST: 'Rust (1.42.0)',
-            PYTHON: 'Python (3.8.2)',
-            DLANG: 'D (DMD 2.091.0)',
-            NIM: 'Nim (1.0.6)',
-            CSHARP: 'C# (Mono-mcs 6.8.0.105)',
-            SWIFT: 'Swift (5.2.1)',
-            GO: 'Go (1.14.1)',
-            JULIA: 'Julia (1.4.0)',
         }
         for language, compiler in language_compiler_map.items():
             self.assertRegex(compiler, language.submission_lang_pattern)
