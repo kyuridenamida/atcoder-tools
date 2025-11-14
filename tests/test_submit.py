e82c45e90e4e1a2fdc6f5be0748c2f5df2f4ca7a
Kazuma Mikami <tyotyo3@gmail.com>
2025-11-15 01:11:19 +0900

    Fix tests

--- tests/test_submit.py
+++ tests/test_submit.py
@@ -7,20 +7,38 @@ RESOURCE_DIR = os.path.abspath(os.path.join(
     "./resources/test_submit/"))
 
 
-def fake_credential_suplier():
-    return "@@fakeuser@@", "fakepass"
-
+def fake_cookie_supplier():
+    from http.cookiejar import Cookie
+    return Cookie(
+        version=0,
+        name='REVEL_SESSION',
+        value="@@@ invalid cookie @@@",
+        port=None,
+        port_specified=False,
+        domain='atcoder.jp',
+        domain_specified=True,
+        domain_initial_dot=False,
+        path='/',
+        path_specified=True,
+        secure=True,
+        expires=None,
+        discard=True,
+        comment=None,
+        comment_url=None,
+        rest={},
+        rfc2109=False
+    )
 
 class TestTester(unittest.TestCase):
 
     def test_submit_fail_when_metadata_not_found(self):
         ok = submit.main(
-            '', ['-d', os.path.join(RESOURCE_DIR, "without_metadata")], fake_credential_suplier, False)
+            '', ['-d', os.path.join(RESOURCE_DIR, "without_metadata")], fake_cookie_supplier, False)
         self.assertFalse(ok)
 
     def test_test_fail(self):
         ok = submit.main(
-            '', ['-d', os.path.join(RESOURCE_DIR, "with_metadata")], fake_credential_suplier, False)
+            '', ['-d', os.path.join(RESOURCE_DIR, "with_metadata")], fake_cookie_supplier, False)
         self.assertFalse(ok)
 
 
