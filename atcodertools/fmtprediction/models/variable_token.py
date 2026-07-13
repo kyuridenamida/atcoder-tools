a8c369e8c784d9e607f12b050b5a5520847d989a
Toshihiro Shimizu <toshihiroshimizu@ToshihironoMac-mini.local>
2026-07-14 01:09:37 +0900

    3次元配列の入力形式推定を追加
    (cherry picked from commit 7f5c88f1bdb9f0566e9aca52fd8d09dadaf87769)

--- atcodertools/fmtprediction/models/variable_token.py
+++ atcodertools/fmtprediction/models/variable_token.py
@@ -1,33 +1,46 @@
 import re
-from typing import Optional, List
+from typing import List, Optional
 
 
-VALID_VAR_NAME_REG_EXP = re.compile("[a-zA-Z_]+")
+VALID_VAR_NAME_REG_EXP = re.compile(
+    "[a-zA-Z_]+"
+)
 
 
 class VariableToken:
-
     """
-    This model is used on the tokenization stage to store variable information with all string indices
-    This class supports up to 2 dimensions.
+    Variable information used during format tokenization.
+
+    Up to three indices are supported.
     """
 
-    def __init__(self, var_name: str, first_index: Optional[str], second_index: Optional[str]):
-        def normalize(x: str):
-            if x is None:
+    def __init__(
+        self,
+        var_name: str,
+        first_index: Optional[str],
+        second_index: Optional[str],
+        third_index: Optional[str] = None,
+    ):
+        def normalize(value):
+            if value is None:
                 return None
-            return x.rstrip(",")
+            return value.rstrip(",")
 
-        def fixed_var_name(x: str):
-            if x[-1] == "_":
-                return x[:-1]
-            return x
+        def fixed_var_name(value):
+            if value.endswith("_"):
+                return value[:-1]
+            return value
 
-        self.var_name = fixed_var_name(normalize(var_name))
+        self.var_name = fixed_var_name(
+            normalize(var_name)
+        )
         self.first_index = normalize(first_index)
         self.second_index = normalize(second_index)
+        self.third_index = normalize(third_index)
 
     def dim_num(self):
+        if self.third_index:
+            return 3
         if self.second_index:
             return 2
         if self.first_index:
@@ -37,14 +50,24 @@ class VariableToken:
     def is_valid(self):
         if not self._has_valid_var_name():
             return False
-        if not self._is_valid_index(self.first_index):
-            return False
-        if not self._is_valid_index(self.second_index):
-            return False
+
+        for index in (
+            self.first_index,
+            self.second_index,
+            self.third_index,
+        ):
+            if not self._is_valid_index(index):
+                return False
+
         return True
 
     def _has_valid_var_name(self):
-        return VALID_VAR_NAME_REG_EXP.fullmatch(self.var_name) is not None
+        return (
+            VALID_VAR_NAME_REG_EXP.fullmatch(
+                self.var_name
+            )
+            is not None
+        )
 
     @staticmethod
     def _is_valid_index(index):
@@ -52,14 +75,19 @@ class VariableToken:
             return True
         if len(index) == 0:
             return False
-        if not index[-1].isalpha() and not index[-1].isdigit():
+        if (
+            not index[-1].isalpha()
+            and not index[-1].isdigit()
+        ):
             return False
-        if index.find(',') != -1:
+        if index.find(",") != -1:
             return False
         return True
 
 
 class TokenizedFormat:
-
-    def __init__(self, var_tokens: List[VariableToken]):
+    def __init__(
+        self,
+        var_tokens: List[VariableToken],
+    ):
         self.var_tokens = var_tokens
--- atcodertools/fmtprediction/predict_simple_format.py
+++ atcodertools/fmtprediction/predict_simple_format.py
@@ -4,7 +4,14 @@ from collections import OrderedDict
 
 from atcodertools.fmtprediction.models.variable import SimpleVariable
 from atcodertools.fmtprediction.models.variable_token import VariableToken
-from atcodertools.fmtprediction.models.format import SingularPattern, Format, ParallelPattern, TwoDimensionalPattern, WrongGroupingError
+from atcodertools.fmtprediction.models.format import (
+    Format,
+    ParallelPattern,
+    SingularPattern,
+    ThreeDimensionalPattern,
+    TwoDimensionalPattern,
+    WrongGroupingError,
+)
 
 
 class UnknownPeriodError(Exception):
@@ -26,33 +33,51 @@ def _predict_period(seq: List[int]):
         return 1
 
 
-def _predict_simple_format_main(var_tokens: List[VariableToken], to_1d_flag=False) -> Format[SimpleVariable]:
+def _predict_simple_format_main(
+    var_tokens: List[VariableToken],
+    to_1d_flag=False,
+) -> Format[SimpleVariable]:
     var_to_positions = {}
     var_to_simple_var = OrderedDict()
 
-    # Pre-computation of the min / max value of each of the first and second
-    # indices.
     for pos, var_token in enumerate(var_tokens):
         var_name = var_token.var_name
 
         if var_name not in var_to_simple_var:
-            var_to_simple_var[var_name] = SimpleVariable.create(
-                var_name, var_token.dim_num())
+            var_to_simple_var[var_name] = (
+                SimpleVariable.create(
+                    var_name,
+                    var_token.dim_num(),
+                )
+            )
             var_to_positions[var_name] = []
 
         var_to_positions[var_name].append(pos)
 
+        if var_token.dim_num() >= 3:
+            var_to_simple_var[
+                var_name
+            ].third_index.update(
+                var_token.third_index
+            )
+
         if var_token.dim_num() >= 2:
-            var_to_simple_var[var_name].second_index.update(
-                var_token.second_index)
+            var_to_simple_var[
+                var_name
+            ].second_index.update(
+                var_token.second_index
+            )
+
         if var_token.dim_num() >= 1:
-            var_to_simple_var[var_name].first_index.update(
-                var_token.first_index)
+            var_to_simple_var[
+                var_name
+            ].first_index.update(
+                var_token.first_index
+            )
 
-    # Building format nodes
     already_processed_vars = set()
+    root = Format()
 
-    root = Format()  # type: Format[SimpleVariable]
     for pos, var_token in enumerate(var_tokens):
         var_name = var_token.var_name
         simple_var = var_to_simple_var[var_name]
@@ -63,32 +88,66 @@ def _predict_simple_format_main(var_tokens: List[VariableToken], to_1d_flag=Fals
         dim = var_token.dim_num()
 
         if dim == 2 and to_1d_flag:
-            simple_var.first_index = simple_var.second_index
+            simple_var.first_index = (
+                simple_var.second_index
+            )
             simple_var.second_index = None
             dim = 1
 
         if dim == 0:
-            root.push_back(SingularPattern(simple_var))
+            root.push_back(
+                SingularPattern(simple_var)
+            )
             already_processed_vars.add(var_name)
+
         elif dim == 1:
-            period = _predict_period(var_to_positions[var_name])
-            parallel_vars_group = [var_to_simple_var[token.var_name]
-                                   for token in var_tokens[pos:pos + period]]
-            try:
-                root.push_back(ParallelPattern(parallel_vars_group))
-            except WrongGroupingError:
-                raise
+            period = _predict_period(
+                var_to_positions[var_name]
+            )
+            parallel_vars_group = [
+                var_to_simple_var[token.var_name]
+                for token in var_tokens[
+                    pos:pos + period
+                ]
+            ]
+
+            root.push_back(
+                ParallelPattern(
+                    parallel_vars_group
+                )
+            )
+
             for var in parallel_vars_group:
-                already_processed_vars.add(var.name)
+                already_processed_vars.add(
+                    var.name
+                )
+
         elif dim == 2:
-            root.push_back(TwoDimensionalPattern(simple_var))
+            root.push_back(
+                TwoDimensionalPattern(
+                    simple_var
+                )
+            )
+            already_processed_vars.add(var_name)
+
+        elif dim == 3:
+            root.push_back(
+                ThreeDimensionalPattern(
+                    simple_var
+                )
+            )
+            already_processed_vars.add(var_name)
+
         else:
             raise NotImplementedError
-        already_processed_vars.add(var_name)
+
     return root
 
 
-def predict_simple_format(var_tokens: List[VariableToken], to_1d_flag=False) -> Format[SimpleVariable]:
+def predict_simple_format(
+    var_tokens: List[VariableToken],
+    to_1d_flag=False,
+) -> Format[SimpleVariable]:
     try:
         return _predict_simple_format_main(var_tokens, to_1d_flag)
     except (WrongGroupingError, UnknownPeriodError):
